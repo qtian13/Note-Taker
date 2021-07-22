08b3c8aa17a0260db49c01fbb4c889470b1a42c0
qtian13 <qiushuang.tian@gmail.com>
2021-07-21 22:48:56 -0700

    add /notes route

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
-// app.listen(process.env.PORT || 3000, function(){
-//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-// });
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
