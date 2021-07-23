ffa3c3038c41d1e19ad92c2523d36e7a6feb22bd
qtian13 <qiushuang.tian@gmail.com>
2021-07-23 16:16:36 -0700

    use /* to deal get request on routes not defined explicitly

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
@@ -104,5 +102,8 @@ app.delete('/api/notes/:id', (req, res) => {
     res.json(response);
 });
 
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
